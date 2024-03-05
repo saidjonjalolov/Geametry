65e68d26fb16bc69071dd450fda77a05db42769e
saidjonjalolov <bmwmf8896@gmail.com>
2024-03-05 21:38:10 +0500

    new project

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿Salom:
+
+Console.WriteLine("punkti tanlang");
+System.Console.WriteLine("1.Asosiy");
+System.Console.WriteLine("2.Chizish");
+
+int tanlash = Convert.ToInt32(Console.ReadLine());
+
+if(tanlash == 1)
+{
+Console.WriteLine("punkti tanlang");
+System.Console.WriteLine("1.Kvadrat peremtri");
+System.Console.WriteLine("2.Uchburchakni peremetri");
+System.Console.WriteLine("3.Tog'ri tortburchak peremetri");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b == 1)
+{
+System.Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(a * 4);
+}
+
+if(b == 2)
+{
+System.Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("d = ");
+int d = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(a + c + d);
+}
+
+if(b == 3)
+{
+System.Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int f = 2*(a + c);
+System.Console.WriteLine(f);
+}
+}
+
+if(tanlash == 2){
+    Console.WriteLine("punkti tanlang");
+System.Console.WriteLine("1.Kvadrat");
+System.Console.WriteLine("2.Uchburchak");
+System.Console.WriteLine("3.Tog'ri tortburchak");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b == 1)
+{
+System.Console.WriteLine("*");
+}
+
+if(b == 2)
+{
+System.Console.WriteLine("*");
+}
+
+if(b == 3)
+{
+System.Console.WriteLine("*");
+}
+}
+
+System.Console.WriteLine("Davom etasizmi. ha yoki yoq");
+string k = Console.ReadLine();
+
+if(k == "ha")
+{
+goto Salom;
+}
+else if(k == "yoq")
+{
+System.Console.WriteLine("ok");
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Geametry
+
+    // -----------------------------------------------
+    // Copyright (c) Coalition of the Good-Hearted Engineers
+    // FREE TO USE TO CONNECT THE WORLD
+    // --------------------------------------------
+
+<h2>This code is geomertic calculator</h2>
+
+> =========================================
+In this code you can solve geometric problems
+
+1. peremitr
+2. picture
+
+> <h2>CODE</h2>
+![CODE](/img/carbon.png)
+
+> <h2>RUN CODE</h2>
+![run code](/img/run%20code.gif)
