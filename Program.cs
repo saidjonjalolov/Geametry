0b280c8ee7fb2497c4e13fab0bc5b1684cd3fbf3
saidjonjalolov <bmwmf8896@gmail.com>
2024-03-06 15:15:45 +0500

    new project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿Salom:
+﻿    // ---------------------------------------------------------------
+    // Copyright (c) Coalition of the Good-Hearted Engineers
+    // FREE TO USE TO CONNECT THE WORLD
+    // ---------------------------------------------------------------
+
+Salom:
 
 Console.WriteLine("punkti tanlang");
 System.Console.WriteLine("1.Asosiy");
@@ -6,76 +11,76 @@ System.Console.WriteLine("2.Chizish");
 
 int tanlash = Convert.ToInt32(Console.ReadLine());
 
-if(tanlash == 1)
+if(tanlash == 1)    
 {
-Console.WriteLine("punkti tanlang");
-System.Console.WriteLine("1.Kvadrat peremtri");
-System.Console.WriteLine("2.Uchburchakni peremetri");
-System.Console.WriteLine("3.Tog'ri tortburchak peremetri");
-
-int b = Convert.ToInt32(Console.ReadLine());
-
-if(b == 1)
-{
-System.Console.Write("a = ");
-int a = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(a * 4);
-}
-
-if(b == 2)
-{
-System.Console.Write("a = ");
-int a = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("c = ");
-int c = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("d = ");
-int d = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(a + c + d);
-}
-
-if(b == 3)
-{
-System.Console.Write("a = ");
-int a = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("c = ");
-int c = Convert.ToInt32(Console.ReadLine());
-int f = 2*(a + c);
-System.Console.WriteLine(f);
-}
-}
-
-if(tanlash == 2){
     Console.WriteLine("punkti tanlang");
-System.Console.WriteLine("1.Kvadrat");
-System.Console.WriteLine("2.Uchburchak");
-System.Console.WriteLine("3.Tog'ri tortburchak");
+    System.Console.WriteLine("1.Kvadrat peremtri");
+    System.Console.WriteLine("2.Uchburchakni peremetri");
+    System.Console.WriteLine("3.Tog'ri tortburchak peremetri");
 
-int b = Convert.ToInt32(Console.ReadLine());
+int tanlash2 = Convert.ToInt32(Console.ReadLine());
 
-if(b == 1)
+if(tanlash2 == 1)
 {
-System.Console.WriteLine("*");
+    System.Console.Write("a = ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(a * 4);
 }
 
-if(b == 2)
+if(tanlash2 == 2)
 {
-System.Console.WriteLine("*");
+    System.Console.Write("a = ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("c = ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("d = ");
+    int d = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(a + c + d);
 }
 
-if(b == 3)
+if(tanlash2 == 3)
 {
-System.Console.WriteLine("*");
+    System.Console.Write("a = ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("c = ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int f = 2*(a + c);
+    System.Console.WriteLine(f);
+}
+}
+
+if(tanlash == 2){   
+    Console.WriteLine("punkti tanlang");
+    System.Console.WriteLine("1.Kvadrat");
+    System.Console.WriteLine("2.Uchburchak");
+    System.Console.WriteLine("3.Tog'ri tortburchak");
+
+int tanlash2 = Convert.ToInt32(Console.ReadLine());
+
+if(tanlash2 == 1)
+{
+    System.Console.WriteLine("********\n*      *\n*      *\n*      *\n********");
+}
+
+if(tanlash2 == 2)
+{
+    Console.WriteLine("       *\n     *  *\n    *    *\n   *      *\n  *        *\n *          *\n**************");
+}
+
+if(tanlash2 == 3)
+{
+    System.Console.WriteLine("*****************\n*      \t\t*\n*      \t\t*\n*      \t\t*\n*****************");
 }
 }
 
 System.Console.WriteLine("Davom etasizmi. ha yoki yoq");
-string k = Console.ReadLine();
+string davometish = Console.ReadLine();
 
-if(k == "ha")
+if(davometish == "ha")   
 {
-goto Salom;
+    goto Salom;
 }
-else if(k == "yoq")
+    else if(davometish == "yoq")
 {
-System.Console.WriteLine("ok");
+    System.Console.WriteLine("ok");
 }
